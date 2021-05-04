e3ebe240501ee8aae4439fa6a1f010d420073aea
Nikhil Karanam <206-nkaranam@users.noreply.newton.msvc.wisc.edu>
2021-05-04 12:39:37 -0500

    added simple omp functionality to jacobi

--- jacobi.cpp
+++ jacobi.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include "omp.h"
 
 void print_arr(float* arr, int n)
 {
@@ -22,34 +23,30 @@ void jacobi(float *A, float *B, float *x, int n)
     const int ITERATION_LIMIT = 1000;
     float LU_sum = 0;
     float * x_new = new float[n];
-    print_arr(x, n);
 
-    for (int k = 0; k < ITERATION_LIMIT; k++)
-    {
-        for (int i = 0 ; i < n; i++)
+    #pragma omp for
+        for (int k = 0; k < ITERATION_LIMIT; k++)
         {
-            LU_sum = 0;
-            for (int j = 0; j < n; j++)
+            for (int i = 0 ; i < n; i++)
             {
-                if (j != i)
-                {
+                LU_sum = 0;
+                for (int j = 0; j < n; j++)
+                {   
                     LU_sum += A[i*n + j]*x[j];
                 }
+                LU_sum -= A[i*n+i]*x[i];
+                x_new[i] = B[i] - LU_sum;
+                x_new[i] = x_new[i]/A[i*n + i];
             }
-            x_new[i] = B[i] - LU_sum;
-            x_new[i] = x_new[i]/A[i*n + i];
+            std::copy(x_new, x_new + n, x);
         }
-        // x = x_new;
-        std::copy(x_new, x_new + n, x);
-        print_arr(x, n);
-
-    }
     delete[] x_new;
 }
 
 int main(int argc, char **argv)
 {
     int n = 4;
+    omp_set_num_threads(2);
 
     using namespace std::chrono;
     high_resolution_clock::time_point start;
@@ -63,6 +60,7 @@ int main(int argc, char **argv)
 	std::random_device entropy_source;
 	std::mt19937_64 generator(entropy_source());
 	std::uniform_real_distribution<float> dist(-1.0,1.0);
+
 	// A[i] = (float)dist(generator);
 
 	for (int i = 0; i < n*n; i++)
@@ -76,43 +74,20 @@ int main(int argc, char **argv)
         B[i] = 1;
     }
 
-    B[0] = 6;
-    B[1] = 25;
-    B[2] = -11;
-    B[3] = 15;
-    
-    A[0] = 10;
-    A[1] = -1;
-    A[2] = 2;
-    A[3] = 0;
-    A[4] = -1;
-    A[5] = 11;
-    A[6] = -1;
-    A[7] = 3;
-    A[8] = 2;
-    A[9] = -1;
-    A[10] = 10;
-    A[11] = -1;
-    A[12] = 0;
-    A[13] = 3;
-    A[14] = -1;
-    A[15] = 8;
-    
-    x[0] = 1;
-    x[1] = 1;
-    x[2] = 1;
-    x[3] = 1;
+    float B_test[4] = {6, 25, -11, 15};
+    float A_test[16] = {10, -1, 2, 0, -1 ,11, -1 ,3 ,2 ,-1 ,10, -1, 0, 3, -1, 8};
+    float x_test[4] = {1, 1, 1, 1};
 
     start = high_resolution_clock::now();
-    jacobi(A,B,x,n);
+    jacobi(A_test, B_test, x_test, n);
     end = high_resolution_clock::now();
 
     duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
    
 
     printf("ms: %f \n", duration_sec.count());
-    printf("Result: \n");
-    print_arr(x,n);
+    printf("Result: ");
+    print_arr(x_test,n);
 
     delete []A;
     delete []B;
