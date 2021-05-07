174dbe5574ba94c7635e154b672432630a6babb9
wchen329 <wchen329@wisc.edu>
2021-05-06 22:02:17 -0500

    lu: decomp

--- lu/lu_solve_cpu.h
+++ lu/lu_solve_cpu.h
@@ -18,21 +18,26 @@ namespace linearstory
 	{
 		public:
 			/* Decompose
-			 * Makes L and U out of A
+			 * Makes L and U out of A (Using Leading Row-Colum LU)
 			 */
 			virtual void Decompose() 
 			{
+				// S: this is a temporary which gets reconstructed every iteration of dim_offset 
+				DataType* S = host_S.get();
+				// S already initalized to A
+
 				// Decompose using iteration 
 				for(size_t dim_offset = 0; dim_offset < dim_pvt; ++dim_offset)
 				{
 					// Set pivots of L and U
-					size_t radial_offset = dim_pvt * dim_offset  + dim_offset;
-
 					DataType* L = host_L.get();
 					DataType* U = host_U.get();
 
+					size_t dim_S = dim_pvt - dim_offset;
+
+					// Constant time
 					L[dim_offset + dim_pvt * dim_offset] = 1;
-					U[dim_offset + dim_pvt * dim_offset] = LinearSystem<DataType>::atA(dim_offset, dim_offset);
+					U[dim_offset + dim_pvt * dim_offset] = S[dim_offset * dim_S + dim_offset];
 
 					// Set this row (dim_offset) and this column to
 					// be the correct values
@@ -40,18 +45,62 @@ namespace linearstory
 					// For all u in U | u has y = dim_offset, u = a
 					for(size_t col = dim_offset + 1; col < dim_pvt; ++col)
 					{
-						U[dim_offset * dim_pvt + col] = LinearSystem<DataType>::atA(dim_offset, col);
+						U[dim_offset * dim_pvt + col] = S[dim_offset * dim_S + col];
 					}
 
 					// For all l in L | l has x = dim_offset, l = a/u
 					for(size_t row = dim_offset + 1; row < dim_pvt; ++row)
 					{
-						// Bad locality... might want to optimize by creating a transpose version of A as well.
-						L[row * dim_pvt + dim_offset] = LinearSystem<DataType>::atA(row, dim_offset) / U[dim_offset * dim_pvt + row];
+						l_buffer.get()[row] = S[row * dim_S + dim_offset] / U[dim_offset * dim_pvt + row];
+						L[row * dim_pvt + dim_offset] = l_buffer.get()[row];
 					}
 
+					// Calculate S
+					// copy the submatrix of A[1:,1:] into S. Reorigin it.
+					size_t counter = 0;
+					for(size_t sy = 1; sy < dim_S; ++sy)
+					{
+						for(size_t sx = 1; sx < dim_S; ++sx)
+						{
+							host_S_tmp.get()[counter] = S[sy * dim_S + sx];
+							++counter;
+						}
+					}
+
+					// Calculate the final value of S.
+					DataType* lptr = l_buffer.get();
+					DataType* uptr = U + (dim_offset * dim_pvt);
+					DataType* hopptr = host_op.get();
+					mat_outer_product(lptr, uptr, hopptr, dim_S - 1);
+
+					// Do matrix subtraction of S_tmp and outer_product
+					mat_sub(host_S_tmp.get(), hopptr, S, dim_S - 1);
 				}
 				
+			}
+
+			void mat_outer_product(DataType* a, DataType* b, DataType* output, size_t rank)
+			{
+				// Calculate the outer product
+				for(size_t i = 0; i < rank; ++i)
+				{
+					for(size_t j = 0; j < rank; ++j)
+					{
+						output[i * rank + j] = a[i] * b[j];
+					}
+				}
+			}
+
+			void mat_sub(DataType * A, DataType* B, DataType* output, size_t rank)
+			{
+				for(size_t i = 0; i < rank; ++i)
+				{
+					for(size_t j = 0; j < rank; ++j)
+					{
+						size_t loc = i * rank + j;
+						output[loc] = A[loc] - B[loc];
+					}
+				}
 			}
 
 			/* forward substitution
@@ -118,6 +167,7 @@ namespace linearstory
 			{
 				#ifdef VERBOSE_DEBUG
 					MatEcho<DataType>(LinearSystem<DataType>::get1D_A_Host(), dim_pvt, dim_pvt);
+					MatEcho<DataType>(host_S.get(), dim_pvt, dim_pvt);
 					MatEcho<DataType>(LinearSystem<DataType>::get1D_B_Host(), 1, dim_pvt);
 				#endif
 
@@ -149,12 +199,20 @@ namespace linearstory
 				host_L(new DataType[dim * dim]),
 				host_U(new DataType[dim * dim]),
 				host_S(new DataType[dim * dim]),
+				host_S_tmp(new DataType[dim * dim]),
+				host_op(new DataType[dim * dim]),
 				host_k(new DataType[dim]),
+				l_buffer(new DataType[dim]),
 				LinearSystem<DataType>(dim)
 			{
 				// Zero Host arrays L and U
 				std::fill_n<DataType*>(host_L.get(), dim * dim, 0);
 				std::fill_n<DataType*>(host_U.get(), dim * dim, 0);
+
+				// Start S as a copy of A
+				std::copy<DataType*>(LinearSystem<DataType>::get1D_A_Host()
+,
+					 LinearSystem<DataType>::get1D_A_Host() + (dim_pvt * dim_pvt), host_S.get());
 
 			}
 
@@ -162,7 +220,10 @@ namespace linearstory
 			MArray host_L;
 			MArray host_U;
 			MArray host_S;
+			MArray host_S_tmp;
 			MArray host_k;
+			MArray host_op;
+			MArray l_buffer;
 		private:
 			size_t dim_pvt;
 	};
